ca7e0631725efb2755503ea639aec0c76241e1ce
Nuno Silva <nuno.m.ribeiro.silva@tecnico.ulisboa.pt>
2016-12-02 06:04:34 +0000

    finding tainted sinks :smile:

--- src/main.py
+++ src/main.py
@@ -8,6 +8,11 @@ def enableDebug():
     logging.info("enabling debug...")
 
 
+# list of tainted entry points
+# lstTaintedEntry[nodetype][nodeId][]
+lstTaintedEntry = {'Variable': {}}
+
+
 # list of tainted sinks
 lstTaintedSinks = []
 
@@ -33,8 +38,7 @@ def isTaintedNode(node, lstEntries, lstValidator, lstSinks):
 
 
 
-
-# this is basically a badly implemented Visitor pattern. However, the Node
+# this is basically a badly implemented Visitor pattern because the Node
 # class just accepts visitor functions, not objects...
 def isNodeTainted(node, lstEntries, lstValidator, lstSinks):
     if isinstance(node, (str, int, float)):
@@ -49,28 +53,39 @@ def isNodeTainted(node, lstEntries, lstValidator, lstSinks):
     if isinstance(node, Variable):
         name = str(node.name)
         logging.debug('testing Variable: ' + name)
-        # TODO: add it to the list of tainted nodes?
-        return (name in lstEntries)
+        try:
+            tainted = isTainted(node)
+        except KeyError:
+            tainted = (name in lstEntries)
+            setTainted(node, tainted)
+        logging.debug('testing Variable: ' + name + ': ' + str(tainted))
+        return tainted
 
     if isinstance(node, Assignment):
+        logging.debug('testing Assignment')
         t = isTaintedNode(node.expr, lstEntries, lstValidator, lstSinks)
-        # TODO: mark node.node as tainted
+        if t:
+            # mark the left value as tainted
+            setTainted(node.node, t)
         # TODO: add it to the list of tainted nodes?
         return t
 
     if isinstance(node, ArrayOffset):
+        logging.debug('testing ArrayOffset')
         t1 = isTaintedNode(node.node, lstEntries, lstValidator, lstSinks)
         t2 = isTaintedNode(node.expr, lstEntries, lstValidator, lstSinks)
         # TODO: add it to the list of tainted nodes? etc
         return t1 or t2
 
     if isinstance(node, BinaryOp):
+        logging.debug('testing BinaryOp')
         t1 = isTaintedNode(node.left, lstEntries, lstValidator, lstSinks)
         t2 = isTaintedNode(node.right, lstEntries, lstValidator, lstSinks)
         # TODO: add it to the list of tainted nodes? etc
         return t1 or t2
 
     if isinstance(node, FunctionCall):
+        logging.debug('testing FunctionCall')
         if node.name in lstValidator:
             # TODO: mark it as not tainted
             return False
@@ -88,12 +103,25 @@ def isNodeTainted(node, lstEntries, lstValidator, lstSinks):
             return taintedArgs
 
     if isinstance(node, Parameter):
+        logging.debug('testing Parameter')
         return isTaintedNode(node.node, lstEntries, lstValidator, lstSinks)
 
     print("Not implemented: " + str(type(node)))
     return False
 
+def setTainted(node, tainted):
+    if isinstance(node, Variable):
+        name = str(node.name)
+        lstTaintedEntry['Variable'][name] = tainted;
+    else:
+        print("Not implemented: setTainted " + str(type(node)))
 
+def isTainted(node):
+    if isinstance(node, Variable):
+        name = str(node.name)
+        return lstTaintedEntry['Variable'][name]
+    else:
+        print("Not implemented: setTainted " + str(type(node)))
 
 
 #if __name__ == "__main__":
